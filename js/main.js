0cc8e6404269182ce71bae98f771c1f32a24d987
PizzerSpring <vladi-slav96@mail.ru>
2024-06-09 14:43:02 +0700

    fix pagination

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
+/*const menu = ['Slide 1', 'Slide 2', 'Slide 3'];*/
 
 const swiper = new Swiper('.swiper', {
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
     },
+    // renderBullet: function (index, className) {
+    //     return '<span class="' + className + '">' + (menu[index]) + '</span>';
+    // },
 });
